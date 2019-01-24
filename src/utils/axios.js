cb528af1e4a306059a95a6f3900faa3596bf930b
wzht1991 <506486189@qq.com>
2019-01-24 17:25:05 +1000

    try to fix appid problem

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const instance=axios.create({
     baseURL:'https://api.openweathermap.org/data/2.5',
     params:{
-        appid:process.env.APPID,
+        APPID:process.env.APPID,
         units:'metric'
     }
 })
